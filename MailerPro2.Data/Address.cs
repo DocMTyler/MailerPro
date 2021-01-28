93e5160022729ff0535596d003f96aeb46fdee14
DocMTyler <tylerhougland@att.net>
2021-01-28 15:24:14 -0500

    Second commit

---
+++ MailerPro2.Data/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailerPro2.Data
+{
+    public class Address
+    {
+        public int ID { get; set; }
+        
+        public string FullName { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+    }
+}
---
+++ MailerPro2.Models/AddressListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailerPro2.Models
+{
+    public class AddressListItem
+    {
+        public string FullName { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+    }
+}
