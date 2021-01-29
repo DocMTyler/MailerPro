d7677abb52dff2577e1759f64488d19bfbd7ee40
DocMTyler <tylerhougland@att.net>
2021-01-29 13:56:09 -0500

    cleaner front end

--- MailerPro2.Data/Address.cs
+++ MailerPro2.Data/Address.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,18 @@ namespace MailerPro2.Data
     public class Address
     {
         public int ID { get; set; }
-        
+
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
 
+        [Display(Name = "Street Address")]
         public string StreetAddress { get; set; }
 
         public string City { get; set; }
 
         public string State { get; set; }
 
+        [Display(Name = "Zip Code")]
         public int ZipCode { get; set; }
 
     }
